6fec9d8f9f08af87e734683cf6190e92eea7c2c7
Djire <odjidev@gmail.com>
2024-06-27 15:01:31 +0000

    test 4

--- Gestion_Spring_Back_end/script.sh
+++ Gestion_Spring_Back_end/script.sh
@@ -1,44 +1,80 @@
-#!/bin/bash
 
-# Install ngx-progressbar package
-npm install ngx-progressbar --save
+#!/usr/bin/env bash
 
-# Change directory to script's location
-cd "$(dirname "$0")"
+# Function to check for Git updates
 
-# Check Git status
-git status
+  # Execute git status and capture output
+  git_status_output=$(git status)
 
-# Add modified files
-git add *
+   git add *
+   git commit -m "Sauvegarde de la version actuelle du local"
+# git pull origin main
 
-# Commit changes with message
-git commit -m "Sauvegarde de la version actuelle"
+  # Check for keywords indicating updates
+  update_keywords=("behind" "diverged" "integrate" "pull")
 
-# Pull updates from remote repository
-git pull origin main
+  # Create a regular expression pattern (case-insensitive)
+  pattern="/\b(${update_keywords[*]})\b/i"
 
-# Check repository cleanliness
-if [ $? -eq 0 ]; then
-  echo "Votre dépôt Git est propre."
-else
-  echo "Votre dépôt Git n'est pas propre. Veuillez exécuter git add et git commit pour enregistrer vos modifications."
+  # Check if updates are present
+  has_updates=$(grep -q "$pattern" <<< "$git_status_output")
 
-  # Add all modified files to Git staging 
-  git add *
+  if [ $has_updates -eq 1 ]; then
+    echo "**Une mise à jour est disponible !**"
+    echo "**Voulez-vous lancer la mise à jour ?**"
+  else
+    echo "**Aucune mise à jour n'est disponible.**"
+  fi
 
-  # Commit changes with specified message
-  git commit -m "Sauvegarde de la version actuelle"
-fi
 
-# Display the most recent commit message
-git log -1 --pretty=format:"%s"
+# # Function to perform Git pull
+# perform_git_pull() {
+#   # Add modified files
+#   git_add_output=$(git add * 2>&1)
 
-# List modified files
-git status | grep 'modified:'
+#   # Commit current changes (optional)
+#   git_commit_output=$(git commit -a -m "Mise à jour Git" 2>&1)
 
-# Confirm successful commit
-echo "Modifications sauvegardées avec succès !"
+#   # Perform Git pull
+#   git_pull_output=$(git pull origin 2>&1)
+
+#   if [ $git_commit_output -eq 0 ] && [ $git_pull_output -eq 0 ]; then
+#     echo "**Mise à jour Git effectuée avec succès !**"
+#     echo "**Vous êtes à jour maintenant.**"
+#   else
+#     echo "**Échec de la mise à jour Git !**"
+#     echo "**Erreurs rencontrées :**"
+#     echo -e "$git_add_output\n$git_commit_output\n$git_pull_output"
+#   fi
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # # Optional Git update checking and pull functionality
 # # Function to check for Git updates
