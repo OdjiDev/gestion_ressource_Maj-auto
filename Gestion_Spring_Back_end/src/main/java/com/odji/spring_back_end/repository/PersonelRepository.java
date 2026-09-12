0f86a79bd5d3abe49dab89bb22477be8e94c16ea
Djire <odjidev@gmail.com>
2026-09-12 15:15:40 +0000

    Mise a niveau de de created at updatedAt et By

--- Gestion_Spring_Back_end/src/main/java/com/odji/spring_back_end/repository/PersonelRepository.java
+++ Gestion_Spring_Back_end/src/main/java/com/odji/spring_back_end/repository/PersonelRepository.java
@@ -12,35 +12,6 @@ import java.util.Optional;
 @Repository
 public interface PersonelRepository extends JpaRepository<Personel, Integer> {
 
-    // ==================== Recherche par matricule/code (si unique) ====================
-
-    Optional<Personel> findByMatricule(String matricule);
-
-    boolean existsByMatricule(String matricule);
-
-    // ==================== Recherche par nom ====================
-
-    List<Personel> findAllByNomContainingIgnoreCase(String nom);
-
-    Page<Personel> findAllByNomContainingIgnoreCase(String nom, Pageable pageable);
-
-    // ==================== Recherche par bureau ====================
-
- //   List<Personel> findAllByBureauId(Integer idBureau);
-
-    //Page<Personel> findAllByBureauId(Integer idBureau, Pageable pageable);
-
-    // ==================== Recherche par département ====================
-
-  //  List<Personel> findAllByDepartementId(Integer idDepartement);
-
-    // ==================== Statistiques ====================
-
-    long countByBureauId(Integer idBureau);
-
-
 
     long countByRoleId(Integer id);
-
-    Optional<Object> findByEmail(String email);
 }
