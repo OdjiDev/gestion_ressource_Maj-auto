41dd4ab63c11d667098f0e56775ca9e2627c5a70
Djire <odjidev@gmail.com>
2026-09-16 07:33:38 +0000

    modification de appRoute pour les routes personnel admin et comptable

--- gestion-ressources-ng22/src/app/app.routes.ts
+++ gestion-ressources-ng22/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { adminGuard } from './core/guards/admin.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+
+  {
+    path: 'admin',
+    canActivate: [authGuard, adminGuard],
+    loadChildren: () => import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES)
+  },
+  {
+    path: 'comptable',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/comptable/comptable.routes').then(m => m.COMPTABLE_ROUTES)
+  },
+  {
+    path: 'personnel',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/personnel/personnel.routes').then(m => m.PERSONNEL_ROUTES)
+  },
+
+  { path: '**', redirectTo: 'login' }
+];
